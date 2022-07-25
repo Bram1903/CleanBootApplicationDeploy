d4b4b84d41d2a37570fc7371d71690614404bfb5
Bram <70259613+Bram1903@users.noreply.github.com>
2022-07-25 05:09:33 +0200

    Check if OS equals Windows

--- installer.ps1
+++ installer.ps1
@@ -1,7 +1,12 @@
 clear
 
+if (!$IsWindows) {
+    Write-Host "Unfornatly this script only works on Windows."
+    Exit
+}
+
 $title = "Bram's Clean Boot Application Deployer"
 $username = $env:UserName
 
 $host.UI.RawUI.WindowTitle = $title
-Write-host "Hello, $username"
+Write-Host "Hello, $username"
