1d0bb35a6209ac44dc135cd4fe3b8f101cd70971
Bram <70259613+Bram1903@users.noreply.github.com>
2022-07-25 05:39:05 +0200

    Added an abort option

--- installer.ps1
+++ installer.ps1
@@ -12,4 +12,24 @@ $title = "Bram's Clean Boot Application Deployer"
 $username = $env:UserName
 
 $host.UI.RawUI.WindowTitle = $title
+
 Write-Host "Hello, $username"
+Write-Host ""
+
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","Will install Chocolatey."
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No","Will NOT install Chocolatey."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+
+
+$title = "Installing chocolatey" 
+$message = "Would you like to install Chocolatey?"
+$result = $host.ui.PromptForChoice($title, $message, $options, 1)
+
+switch ($result) {
+    0{
+        Write-Host "Started the installation of Chocolatey." -ForegroundColor Green
+    }1{
+        Write-Host "Installation aborted." -ForegroundColor Red
+        Exit
+    }
+}
