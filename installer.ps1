af1ace4b0cb9d6d60238bfd2b6d6cc3efd1a4419
Bram <70259613+Bram1903@users.noreply.github.com>
2022-07-25 06:17:06 +0200

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -42,3 +42,6 @@ switch ($result) {
 }
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+Write-Host "Chocolatey should be succesfully installed." -ForegroundColor Green
+Write-Host "Starting the installation of the packages..."
